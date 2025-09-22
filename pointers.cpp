f332a57c49976ae932aa569304d963721f4d6a15
sosoobinary <quangxngo04@gmail.com>
2025-09-21 20:00:18 -0700

    10 min

--- pointers.cpp
+++ pointers.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+
 using namespace std;
+
+struct Customer {
+    int age;
+    string* drinks;
+
+    ~Customer() {
+        if (drinks) {
+            delete [] drinks;
+        }
+        drinks = nullptr;
+    }
+};
+
